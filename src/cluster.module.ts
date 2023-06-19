df7ba5712bbcac9d12927989c5bcfce976758de9
Alko89 <alefer89@gmail.com>
2023-06-19 16:01:49 +0200

    fix: don't close connections in module

--- src/cluster.module.ts
+++ src/cluster.module.ts
@@ -1,7 +1,5 @@
-import { DynamicModule, Logger, Module, OnModuleDestroy } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { CLUSTER_CLIENT, CLUSTER_MODULE_OPTIONS } from './cluster.constants';
-import { Cluster } from 'ioredis';
-import { InjectRedis } from './injectRedis.decorator';
 import { createClusterProvider } from './cluster.provider';
 import { ClusterConfig } from './cluster.interface';
 
@@ -9,21 +7,12 @@ import { ClusterConfig } from './cluster.interface';
   providers: [createClusterProvider()],
   exports: [CLUSTER_CLIENT],
 })
-export class ClusterModule implements OnModuleDestroy {
-  private readonly logger = new Logger(this.constructor.name);
-
-  constructor(@InjectRedis() private readonly clusterClient: Cluster) {}
-
+export class ClusterModule {
   static register(options: ClusterConfig): DynamicModule {
     return {
       module: ClusterModule,
       global: options.isGlobal,
       providers: [{ provide: CLUSTER_MODULE_OPTIONS, useValue: options }],
     };
-  }
-
-  onModuleDestroy() {
-    this.logger.verbose('Closing Cluster connection');
-    this.clusterClient.disconnect();
   }
 }
--- src/redis.module.ts
+++ src/redis.module.ts
@@ -1,29 +1,18 @@
-import { DynamicModule, Logger, Module, OnModuleDestroy } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { REDIS_CLIENT, REDIS_MODULE_OPTIONS } from './redis.constants';
 import { RedisConfig } from './redis.interface';
 import { createRedisProvider } from './redis.provider';
-import { Redis } from 'ioredis';
-import { InjectRedis } from './injectRedis.decorator';
 
 @Module({
   providers: [createRedisProvider()],
   exports: [REDIS_CLIENT],
 })
-export class RedisModule implements OnModuleDestroy {
-  private readonly logger = new Logger(this.constructor.name);
-
-  constructor(@InjectRedis() private readonly redisClient: Redis) {}
-
+export class RedisModule {
   static register(options: RedisConfig): DynamicModule {
     return {
       module: RedisModule,
       global: options.isGlobal,
       providers: [{ provide: REDIS_MODULE_OPTIONS, useValue: options }],
     };
-  }
-
-  onModuleDestroy() {
-    this.logger.verbose('Closing Redis connection');
-    this.redisClient.disconnect();
   }
 }

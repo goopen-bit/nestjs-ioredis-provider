d62b9ce8beb361d25aa34df3967508025de0e53f
Alko89 <alefer89@gmail.com>
2024-05-04 11:43:48 +0200

    fix: close redis connection on module destroy

--- src/cluster.module.ts
+++ src/cluster.module.ts
@@ -1,18 +1,26 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, OnModuleDestroy } from '@nestjs/common';
 import { CLUSTER_CLIENT, CLUSTER_MODULE_OPTIONS } from './cluster.constants';
 import { createClusterProvider } from './cluster.provider';
 import { ClusterConfig } from './cluster.interface';
+import { InjectCluster } from './injectCluster.decorator';
+import { Cluster } from 'ioredis';
 
 @Module({
   providers: [createClusterProvider()],
   exports: [CLUSTER_CLIENT],
 })
-export class ClusterModule {
+export class ClusterModule implements OnModuleDestroy {
+  constructor(@InjectCluster() private readonly cluster: Cluster) {}
+
   static register(options: ClusterConfig): DynamicModule {
     return {
       module: ClusterModule,
       global: options.isGlobal,
       providers: [{ provide: CLUSTER_MODULE_OPTIONS, useValue: options }],
     };
+  }
+
+  async onModuleDestroy() {
+    await this.cluster.quit();
   }
 }
--- src/redis.module.ts
+++ src/redis.module.ts
@@ -1,18 +1,26 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, OnModuleDestroy } from '@nestjs/common';
+import Redis from 'ioredis';
 import { REDIS_CLIENT, REDIS_MODULE_OPTIONS } from './redis.constants';
 import { RedisConfig } from './redis.interface';
 import { createRedisProvider } from './redis.provider';
+import { InjectRedis } from './injectRedis.decorator';
 
 @Module({
   providers: [createRedisProvider()],
   exports: [REDIS_CLIENT],
 })
-export class RedisModule {
+export class RedisModule implements OnModuleDestroy {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
   static register(options: RedisConfig): DynamicModule {
     return {
       module: RedisModule,
       global: options.isGlobal,
       providers: [{ provide: REDIS_MODULE_OPTIONS, useValue: options }],
     };
+  }
+
+  async onModuleDestroy() {
+    await this.redis.quit();
   }
 }
